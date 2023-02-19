5147ae4da11039366a9ac8abc07137d206e4b7f5
lusinehpetrosian <125768884+lusinehpetrosian@users.noreply.github.com>
2023-02-19 18:08:34 +0400

    Add files via upload

---
+++ Homework1_uni.py
@@ -0,0 +1,26 @@
+#1
+'''
+import sys
+count = len(sys.argv[1])
+array = list(sys.argv[1])
+sum = 0
+for i in range(count):
+     sum += int(array[i])
+print (sum)
+#2
+import sys
+count = int(sys.arg[1])
+k = count-1
+p = 1
+for i in range(count):
+    print(k * ' '  + p * '#')
+    p += 1
+    k -= 1
+'''
+
+
+
+
+
+
+    
