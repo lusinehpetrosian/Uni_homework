b206f3cd2b2172b0d4dc103b4d1b200040a95d44
lusinehpetrosian <125768884+lusinehpetrosian@users.noreply.github.com>
2023-02-22 23:54:28 +0400

    Add files via upload

---
+++ Homework2_uni.py
@@ -0,0 +1,40 @@
+#1
+# nums = [1,2,3,4,5,6,7,8]
+# k = int(input())%len(nums)
+# i = len(nums)
+# for r in range(k):
+#     digit = nums[i-1]
+#     nums.pop(i-1)
+#     nums = [digit] + nums
+# print(nums)
+
+
+#2
+# nums1 = [1,2,4,5,5,6]
+# nums2 = [1,3,5,7,9]
+# nums1s = set(nums1)
+# nums2s = set(nums2)
+# print(nums1s & nums2s)
+
+#3
+# nums = [1,2,3,4,5,6,7]
+# newlist = []
+# s = 0
+# for i in range(len(nums)):
+#     s+=nums[i]
+#     newlist.append(s)
+# print (newlist)
+
+# #4?????
+# s = input()
+# stack = []
+# for i in range(len(s)):
+#     if (s[i] == '{' or s[i] == '[' or s[i] == '('):
+#         stack.append(s[i])
+#     elif (s[i] == '}' or s[i] == ']' or s[i] == ')'):
+#         if(stack == []):
+#             print("They are not equal!")
+#             break;
+#         elif (stack =! []):
+    
+
